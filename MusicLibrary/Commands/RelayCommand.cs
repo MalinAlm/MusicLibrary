053f6e6caeb5dde18edef7e6dbc0fb948fb53de7
MalinAlm <malin.therese.almqvist@gmail.com>
2025-12-11 15:34:04 +0100

    add project structure add MVVM structure, BaseViewModel, RelayCommand

---
+++ MusicLibrary/Commands/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MusicLibrary.Commands
+{
+    class RelayCommand : ICommand
+    {
+
+            private readonly Action<object?> execute;
+            private readonly Func<object?, bool>? canExecute;
+
+            public event EventHandler? CanExecuteChanged;
+
+            public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+
+
+            public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+            {
+                ArgumentNullException.ThrowIfNull(execute);
+                this.execute = execute;
+                this.canExecute = canExecute;
+            }
+            public bool CanExecute(object? parameter) => canExecute is null ? true : canExecute(parameter);
+
+
+            public void Execute(object? parameter) => execute(parameter);
+
+        
+    }
+}
