9543ffa6c41e3fe1d1ed295b3f9653dc360ceccd
MalinAlm <malin.therese.almqvist@gmail.com>
2025-12-11 14:20:03 +0100

    add server connection, install EF package

---
+++ MusicLibrary/Model/LibraryContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Model
+{
+    class LibraryContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = new SqlConnectionStringBuilder()
+            {
+                ServerSPN = "localhost",
+                InitialCatalog = "everyloop",
+                TrustServerCertificate = true,
+                IntegratedSecurity = true,
+            }.ToString();
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
